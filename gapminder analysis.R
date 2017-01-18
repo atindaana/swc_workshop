9ba8a0071a2ad1d426b991fddc6512e5facb8bbf
Edmond A. Atindaana <atindaan@umich.edu>
2017-01-18 10:43:33 -0500

    gapminder\ analysis.R

---
+++ gapminder analysis.R
@@ -0,0 +1,19 @@
+download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv", destfile = "gapminder-FiveYearData.csv")
+gapminder <- read.csv("gapminder-FiveYearData.csv")
+#head
+#ncol
+#nrow
+#summary
+#view
+is_africa <- gapminder@continent == "Africa"instal
+is_2007 <- gapfinder$year == 2007
+africa_2007 <- gapminder [is_2007 & is_africa, c("country","lifeExp")]
+#how to make plots with ggplot2
+ggplot(data=gapminder, aes(x=gdpPercap, y=lifeExp))+geom_point()
+ggplot(data=gapminder, aes(x=year, y=lifeExp))+geom_point()
+#plotting with points on top of the lines
+ggplot(data=gapminder, aes(x=year, y=lifeExp, by = country, color = continent))+geom_line()+geom_point()
+#to get the size to be as big as the gdpPercap
+ggplot(data=gapminder, aes(x=year, y=lifeExp, size =gdpPercap, by = country, color = continent))+geom_line()+geom_point()
+#to add line color line
+ggplot(data=gapminder, aes(x=year, y=lifeExp, size =gdpPercap, by = country, color = continent))+geom_line(color="black")+geom_point(aes(size=gdpPercap))
